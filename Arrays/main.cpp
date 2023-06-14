3aa7eea22d4a66f1370c601c0d757e51f74d43df
Machk_yz84@top-academy.ru <Machk_yz84@top-academy.ru>
2023-06-14 15:46:52 +0300

    Overloading DONE

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -4,21 +4,47 @@ using namespace std;
 #define tab "\t"
 
 void FillRand(int arr[], const int size);
+void FillRand(double arr[], const int size);
+void FillRand(char arr[], const int size);
+
 void Print(const int arr[], const int SIZE);
+void Print(const double arr[], const int SIZE);
+void Print(const char arr[], const int SIZE);
+
 void Sort(int arr[], int SIZE);
+void Sort(double arr[], int SIZE);
+void Sort(char arr[], int SIZE);
+
 int Sum(const int arr[], const int SIZE);
-double Avg(const int arr[], const int SIZE);
+double Sum(const double arr[], const int SIZE);
+char Sum(const char arr[], const int SIZE);
+
+int Avg(const int arr[], const int SIZE);
+double Avg(const double arr[], const int SIZE);
+char Avg(const char arr[], const int SIZE);
+
 int minValueIn(const int arr[], const int SIZE);
+double minValueIn(const double arr[], const int SIZE);
+char minValueIn(const char arr[], const int SIZE);
+
 int maxValueIn(const int arr[], const int SIZE);
+double maxValueIn(const double arr[], const int SIZE);
+char maxValueIn(const char arr[], const int SIZE);
+
 void shiftLeft(int arr[], int SIZE);
+void shiftLeft(double arr[], int SIZE);
+void shiftLeft(char arr[], int SIZE);
+
 void shiftRight(int arr[], int SIZE);
+void shiftRight(double arr[], int SIZE);
+void shiftRight(char arr[], int SIZE);
 
 
 void main() {
 	setlocale(LC_ALL, "");
 
 	const int SIZE = 10;
-	int arr[SIZE];
+	char arr[SIZE];
 
 	//Заполнение массива случайными числами
 	FillRand(arr, SIZE);
@@ -46,7 +72,7 @@ void main() {
 	shiftLeft(arr, SIZE);
 	Print(arr, SIZE);
 
-	//Cдвигает массив на заданное число элементов вправо
+	////Cдвигает массив на заданное число элементов вправо
 	shiftRight(arr, SIZE);
 	Print(arr, SIZE);
 
@@ -60,7 +86,39 @@ void FillRand(int arr[], const int size) {
 	}
 }
 
+void FillRand(double arr[], const int size) {
+	for (int i = 0; i < size; i++)
+	{
+		arr[i] = rand() % 10000;
+		arr[i] /= 100;
+	}
+}
+
+void FillRand(char arr[], const int size) {
+	for (int i = 0; i < size; i++)
+	{
+		arr[i] = rand() % 10000;
+		arr[i] /= 100;
+	}
+}
+
 void Print(const int arr[], const int SIZE) {
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;	
+}
+
+void Print(const double arr[], const int SIZE) {
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;	
+}
+
+void Print(const char arr[], const int SIZE) {
 	for (int i = 0; i < SIZE; i++)
 	{
 		cout << arr[i] << tab;
@@ -83,6 +141,36 @@ void Sort(int arr[], int SIZE) {
 	}
 }
 
+void Sort(double arr[], int SIZE) {
+	for (int i = 0; i < SIZE; i++)
+	{
+		for (int j = i + 1; j < SIZE; j++)
+		{
+			if (arr[i] > arr[j])
+			{
+				double buffer = arr[j];
+				arr[j] = arr[i];
+				arr[i] = buffer;
+			}
+		}
+	}
+}
+
+void Sort(char arr[], int SIZE) {
+	for (int i = 0; i < SIZE; i++)
+	{
+		for (int j = i + 1; j < SIZE; j++)
+		{
+			if (arr[i] > arr[j])
+			{
+				char buffer = arr[j];
+				arr[j] = arr[i];
+				arr[i] = buffer;
+			}
+		}
+	}
+}
+
 int Sum(const int arr[], const int SIZE) {
 	int sum = 0;
 	for (int i = 0; i < SIZE; i++)
@@ -92,7 +180,35 @@ int Sum(const int arr[], const int SIZE) {
 	return sum;
 }
 
-double Avg(const int arr[], const int SIZE) {
+double Sum(const double arr[], const int SIZE) {
+	double sum = 0;
+	for (int i = 0; i < SIZE; i++)
+	{
+		sum += arr[i];
+	}
+	return sum;
+}
+
+char Sum(const char arr[], const int SIZE) {
+	char sum = 0;
+	for (int i = 0; i < SIZE; i++)
+	{
+		sum += arr[i];
+	}
+	return sum;
+}
+
+int Avg(const int arr[], const int SIZE) {
+	
+	return (double)Sum(arr, SIZE) / SIZE;
+}
+
+double Avg(const double arr[], const int SIZE) {
+	
+	return (double)Sum(arr, SIZE) / SIZE;
+}
+
+char Avg(const char arr[], const int SIZE) {
 	
 	return (double)Sum(arr, SIZE) / SIZE;
 }
@@ -109,8 +225,56 @@ int maxValueIn(const int arr[], const int SIZE) {
 	return max;
 }
 
+double maxValueIn(const double arr[], const int SIZE) {
+	double max = 0;
+	for (int i = 0; i < SIZE; i++)
+	{
+		if (max < arr[i])
+		{
+			max = arr[i];
+		}
+	}
+	return max;
+}
+
+char maxValueIn(const char arr[], const int SIZE) {
+	char max = 0;
+	for (int i = 0; i < SIZE; i++)
+	{
+		if (max < arr[i])
+		{
+			max = arr[i];
+		}
+	}
+	return max;
+}
+
 int minValueIn(const int arr[], const int SIZE) {
 	int min = arr[0];
+	for (int i = 0; i < SIZE; i++)
+	{
+		if (min > arr[i])
+		{
+			min = arr[i];
+		}
+	}
+	return min;
+}
+
+double minValueIn(const double arr[], const int SIZE) {
+	double min = arr[0];
+	for (int i = 0; i < SIZE; i++)
+	{
+		if (min > arr[i])
+		{
+			min = arr[i];
+		}
+	}
+	return min;
+}
+
+char minValueIn(const char arr[], const int SIZE) {
+	char min = arr[0];
 	for (int i = 0; i < SIZE; i++)
 	{
 		if (min > arr[i])
@@ -135,12 +299,70 @@ void shiftLeft(int arr[], int SIZE) {
 	}
 }
 
+void shiftLeft(double arr[], int SIZE) {
+	int number_of_shifts;
+	cout << "Введите число сдвига: "; cin >> number_of_shifts;
+	for (int i = 0; i < number_of_shifts; i++)
+	{
+		double buffer = arr[0];
+		for (int i = 0; i < SIZE; i++)
+		{
+			arr[i] = arr[i + 1];
+		}
+		arr[SIZE - 1] = buffer;
+	}
+}
+
+void shiftLeft(char arr[], int SIZE) {
+	int number_of_shifts;
+	cout << "Введите число сдвига: "; cin >> number_of_shifts;
+	for (int i = 0; i < number_of_shifts; i++)
+	{
+		double buffer = arr[0];
+		for (int i = 0; i < SIZE; i++)
+		{
+			arr[i] = arr[i + 1];
+		}
+		arr[SIZE - 1] = buffer;
+	}
+}
+
 void shiftRight(int arr[], int SIZE) {
 	int number_of_shifts;
 	cout << "Введите число сдвига: "; cin >> number_of_shifts;
 	for (int i = 0; i < number_of_shifts; i++)
 	{
 		int buffer = arr[SIZE - 1];
+		for (int i = SIZE - 2; i >= 0; i--)
+		{
+			arr[i + 1] = arr[i];
+		}
+		arr[0] = buffer;
+	}
+
+}
+
+void shiftRight(double arr[], int SIZE) {
+	int number_of_shifts;
+	cout << "Введите число сдвига: "; cin >> number_of_shifts;
+	for (int i = 0; i < number_of_shifts; i++)
+	{
+		double buffer = arr[SIZE - 1];
+		for (int i = SIZE - 2; i >= 0; i--)
+		{
+			arr[i + 1] = arr[i];
+		}
+		arr[0] = buffer;
+	}
+
+}
+
+void shiftRight(char arr[], int SIZE) {
+	int number_of_shifts;
+	cout << "Введите число сдвига: "; cin >> number_of_shifts;
+	for (int i = 0; i < number_of_shifts; i++)
+	{
+		double buffer = arr[SIZE - 1];
 		for (int i = SIZE - 2; i >= 0; i--)
 		{
 			arr[i + 1] = arr[i];
