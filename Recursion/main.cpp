3c8dbe1d636d7ef741218b9652eec39e966ef67f
Machk_yz84@top-academy.ru <Machk_yz84@top-academy.ru>
2023-06-26 13:38:22 +0300

    Recursion DONE

--- Recursion/main.cpp
+++ Recursion/main.cpp
@@ -5,12 +5,14 @@ using namespace std;
 //#define ELEVATOR
 //#define FACTORIAL
 //#define POWER
+#define FIBONACCI
 
 #define tab "\t"
 
 void elevator(int floor);
 int factorial(int number);
-int power(int number, int degree);
+double power(int number, int degree);
+void fibonacci(long long int n, long long int a = 0, long long int b = 1);
 
 void main() {
 	setlocale(LC_ALL, "");
@@ -19,6 +21,7 @@ void main() {
 	int number, degree;
 	cout << "¬ведите число: "; cin >> number;
 	cout << "¬ведите степень: "; cin >> degree;
+	cout << power(number, degree);
 #endif // POWER
 
 
@@ -35,20 +38,31 @@ void main() {
 	elevator(floor);
 #endif // ELEVATOR
 
+#ifdef FIBONACCI
+	int n;
+	cout << "¬ведите предельное число: "; cin >> n;
+	fibonacci(n);
+#endif // FIBONACCI
+
 
 }
 
 int factorial(int number) {
-	if (number == 0 || number == 1)
-	{
-		return 1;
-	}
-
-	return number * factorial(number - 1);
+	return number == 0 ? 1 : number * factorial(number - 1);
 }
 
+double power(int number, int degree) {
+	//if (degree == 0) return 1;
+	//else if (degree > 0) return number * power(number, degree - 1);
+	//else return 1 / power(number, -degree);
+	return degree == 0 ? 1 : degree > 0 ? number * power(number, degree - 1) : 1 / power(number, -degree);
+}
 
-
+void fibonacci(long long int n, long long int a, long long int b) {
+	if (a > n)return;
+	cout << a << tab;
+	fibonacci(n, b, a + b);
+}
 
 void elevator(int floor) {
 	if (floor == 0) {
